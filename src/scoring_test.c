a666502f4f1b5d1bf223fc581f577b10456ca03a
awmc000 <mccolmalex@gmail.com>
2023-08-21 17:31:34 -0700

    removed tests for bad lemmatize function

--- src/scoring_test.c
+++ src/scoring_test.c
@@ -2,21 +2,15 @@
 #include "test_utils.h"
 #include <stdio.h>
 
-/*
-void print_score_bracket(double dale_chall_score);
-double assess_readability(FILE *text_file);
-*/
-
 // Test case forward declarations
 
 /*
- 4th grade
-5th or 6th grade
-7th or 8th grade
-9th or 10th grade
-11th or 12th grade
-college
-college graduate
+ *	4.9 or lower 		4th-grade student or lower
+ *	5.0–5.9 			5th- or 6th-grade student
+ *	6.0–6.9 			7th- or 8th-grade student
+ *	7.0–7.9 			9th- or 10th-grade student
+ *	8.0–8.9 			11th- or 12th-grade student
+ *	9.0–9.9 			college student 
 */
 
 int assess_roysullivan3();
@@ -26,21 +20,15 @@ int assess_raisedbydogs4();
 int assess_alchemy5();
 int assess_nomads6();
 
-int strip_plural_markers();
-int strip_past_tense();
 // Main method runs test cases printing results with announce_test
 int main(void)
 {
 	int test_num = 0;
 
-	// announce_test(test_num, "Creating a HT", &can_create); 
 	RUN_TEST(assess_roysullivan3)
 
 	RUN_TEST(assess_electoralcollege3)
 
-	RUN_TEST(strip_plural_markers)
-
-	RUN_TEST(strip_past_tense)
 	return 0;
 }
 
@@ -88,40 +76,4 @@ int assess_nomads6()
 {
 	// TODO: IMPLEMENT
 	return 1;
-}
-
-int strip_plural_markers()
-{
-	int all_correct = 1;
-	char plurals[][6] = {"roots", "docks", "bones"};
-	char singles[][5] = {"root", "dock", "bone"};
-	
-	for (int i = 0; i < 3; i++)
-	{
-
-		if ( strcmp(drop_end_if_s(plurals[i]), singles[i]) != 0 )
-			all_correct = 0;
-		else
-			printf("converted %s to %s\n", plurals[i], singles[i]);
-	}
-
-	return all_correct;
-}
-
-int strip_past_tense()
-{
-	int all_correct = 1;
-	char plurals[][7] = {"rooted", "docked", "picked"};
-	char singles[][5] = {"root", "dock", "pick"};
-	
-	for (int i = 0; i < 3; i++)
-	{
-
-		if ( strcmp(drop_end_if_s(plurals[i]), singles[i]) != 0 )
-			all_correct = 0;
-		else
-			printf("converted %s to %s\n", plurals[i], singles[i]);
-	}
-
-	return all_correct;
 }
