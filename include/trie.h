b6618d9458c1e943ca9b10e60299d05a3cc9363d
awmc000 <mccolmalex@gmail.com>
2024-01-16 19:55:58 -0800

    doc comments for Trie

--- include/trie.h
+++ include/trie.h
@@ -13,13 +13,37 @@
 
 struct TrieNode
 {
-	struct TreeNode * links[ALPHABET_SIZE];
+	struct TrieNode * links[ALPHABET_SIZE];
 	unsigned int num_links: 4;
 	unsigned int is_terminal : 1; // bit flag
 };
 
-struct TrieNode * create_trie();
+/**
+ * @brief Allocates a TrieNode on the heap.
+ **/
+struct TrieNode * create_trie_node();
+
+/**
+ * @brief Tries to navigate to the last char in a string.
+ * @param str Search key.
+ * @return Pointer to the TrieNode representing the last letter
+ * in the string or NULL if not found.
+ **/
+struct TrieNode * subtrie(const char * search_key);
+
+/**
+ * @brief Adds a string to a Trie.
+ * @param trie Root node of trie which does not represent any letter.
+ * @return 1 if inserted and 0 on error.
+ **/
 int trie_insert(struct TrieNode * trie, const char * value);
+
+
+/**
+ * @brief Checks if the Trie contains a string.
+ * @param trie Root node of trie which does not represent any letter.
+ * @return 1 if found and 0 if not found or error.
+ **/
 int trie_contains(struct TrieNode * trie, const char * search_key);
 
 #endif // TRIE_H
